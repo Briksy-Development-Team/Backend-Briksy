25c7d6f371d506f747eba1838af32f883009ea5e
KV448 <kalyani.vyas@elsolve.com>
2026-03-20 14:59:43 +0530

    refactor(api): update seeker routes to remove versioning and unused inquiry endpoint

--- routes/api.php
+++ routes/api.php
@@ -1,14 +1,13 @@
 <?php
 
 use App\Http\Controllers\Api\Seeker\FavoriteController;
-use App\Http\Controllers\Api\Seeker\InquiryController;
 use App\Http\Controllers\Api\Seeker\OrganizationSearchController;
 use App\Http\Controllers\Api\Seeker\PropertySearchController;
 use App\Http\Controllers\Api\Seeker\RegistrationController;
 use App\Http\Controllers\Api\Seeker\ReviewController;
 use Illuminate\Support\Facades\Route;
 
-Route::prefix('v1/seeker')->group(function (): void {
+Route::prefix('seeker')->group(function (): void {
     Route::post('auth/register', [RegistrationController::class, 'store']);
 
     Route::get('properties', [PropertySearchController::class, 'index']);
@@ -17,7 +16,6 @@ Route::prefix('v1/seeker')->group(function (): void {
     Route::get('organizations', [OrganizationSearchController::class, 'index']);
     Route::get('organizations/{organization}', [OrganizationSearchController::class, 'show']);
 
-    Route::post('inquiries', [InquiryController::class, 'store']);
     Route::get('favorites', [FavoriteController::class, 'index']);
     Route::post('favorites', [FavoriteController::class, 'store']);
     Route::delete('favorites/{favorite}', [FavoriteController::class, 'destroy']);
